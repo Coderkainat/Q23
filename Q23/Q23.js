c56e5247318870eb8635ced368b6d880bb79cd57
Coderkainat <159674299+Coderkainat@users.noreply.github.com>
2024-03-14 05:39:59 +0500

    Add files via upload

---
+++ Q23/Q23.js
@@ -0,0 +1,36 @@
+// conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+var car1 = "subaru";
+console.log("is car == `subaru`? i predict true.");
+console.log(car1 == "subaru");
+var car7 = "civic";
+console.log("is car == `subaru`? i predict true.");
+console.log(car7 == "civic");
+var car8 = "civic";
+console.log("is car == `subaru`? i predict true.");
+console.log(car8 == "civic");
+var car9 = "civic";
+console.log("is car == `subaru`? i predict true.");
+console.log(car9 == "civic");
+var car10 = "civic";
+console.log("is car == `subaru`? i predict true.");
+console.log(car10 == "civic");
+var car2 = "suzuki";
+console.log("is car == `subaru`? i predict true.");
+console.log(car2 == "subaru");
+var car3 = "civic";
+console.log("is car == `subaru`? i predict true.");
+console.log(car3 == "subaru");
+var car4 = "Honda";
+console.log("is car == `subaru`? i predict true.");
+console.log(car4 == "subaru");
+var car5 = "civic";
+console.log("is car == `subaru`? i predict true.");
+console.log(car5 == "subaru");
+var car6 = "toyota";
+console.log("is car == `subaru`? i predict true.");
+console.log(car6 == "subaru");
---
+++ Q23/Q23.ts
@@ -0,0 +1,86 @@
+// conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+// let car = 'subaru';
+
+// console.log("Is car == 'subaru'? I predict True.")
+
+// console.log(car == 'subaru')
+
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+let car1 = `subaru`;
+
+console.log("is car == `subaru`? i predict true.")
+
+console.log( car1 == `subaru`)
+
+
+let car7 = `civic`;
+
+console.log("is car == `subaru`? i predict true.")
+
+console.log( car7== `civic`)
+
+
+let car8 = `civic`;
+
+console.log("is car == `subaru`? i predict true.")
+
+console.log( car8== `civic`)
+
+
+
+let car9 = `civic`;
+
+console.log("is car == `subaru`? i predict true.")
+
+console.log( car9 == `civic`)
+
+
+
+let car10 = `civic`;
+
+console.log("is car == `subaru`? i predict true.")
+
+console.log( car10== `civic`)
+
+
+
+let car2 = `suzuki`;
+
+console.log("is car == `subaru`? i predict true.")
+
+console.log( car2 == `subaru`)
+
+let car3 = `civic`;
+
+console.log("is car == `subaru`? i predict true.")
+
+console.log( car3== `subaru`)
+
+
+let car4 = `Honda`;
+
+console.log("is car == `subaru`? i predict true.")
+
+console.log( car4 == `subaru`)
+
+
+let car5 = `civic`;
+
+console.log("is car == `subaru`? i predict true.")
+
+console.log( car5 == `subaru`)
+
+
+
+let car6 = `toyota`;
+
+console.log("is car == `subaru`? i predict true.")
+
+console.log( car6== `subaru`)
+
+
+
